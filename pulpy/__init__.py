92e8c4253d01a1aabe23c94bfb76726849095e04
jonbmartin <jon.bach.martin@gmail.com>
2024-02-08 14:46:52 -0600

    initial copy over of sigpy.rf functionality

--- pulpy/__init__.py
+++ pulpy/__init__.py
@@ -1,1 +1,47 @@
 
+"""The core module contains functions and classes for RF pulse design.
+
+It contains functions to design a variety of RF pulses for MRI, such as SLR,
+adiabatic, parallel transmit, multibanded, and others. The submodule also
+includes other functions to assist with pulse design, such as I/O functions,
+trajectory/gradient designers, and Bloch simulators.
+
+"""
+from pulpy import (
+    adiabatic,
+    b1sel,
+    io,
+    multiband,
+    optcont,
+    ptx,
+    shim,
+    sim,
+    slr,
+    trajgrad,
+    util,
+)
+from pulpy.adiabatic import *  # noqa
+from pulpy.b1sel import *  # noqa
+from pulpy.io import *  # noqa
+from pulpy.linop import *  # noqa
+from pulpy.multiband import *  # noqa
+from pulpy.optcont import *  # noqa
+from pulpy.ptx import *  # noqa
+from pulpy.shim import *  # noqa
+from pulpy.sim import *  # noqa
+from pulpy.slr import *  # noqa
+from pulpy.trajgrad import *  # noqa
+from pulpy.util import *  # noqa
+
+__all__ = ["linop"]
+__all__.extend(adiabatic.__all__)
+__all__.extend(b1sel.__all__)
+__all__.extend(io.__all__)
+__all__.extend(multiband.__all__)
+__all__.extend(optcont.__all__)
+__all__.extend(ptx.__all__)
+__all__.extend(sim.__all__)
+__all__.extend(shim.__all__)
+__all__.extend(slr.__all__)
+__all__.extend(trajgrad.__all__)
+__all__.extend(util.__all__)
--- pulpy/example.py
+++
@@ -1,8 +0,0 @@
-def add_one(number):
-    return number + 1
-
-def fib(number):    
-    if number <= 1:
-        return number
-    else:
-        return fib(number-1) + fib(number-2)
