73e789fb7071c6924634351721ab8b637ed9ffdd
jonbmartin <jon.bach.martin@gmail.com>
2024-02-13 15:42:58 -0600

    update version to 1.7.0

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "Jonathan Martin"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "1.6.3"
+release = "1.7.0"
 
 
 # -- General configuration ---------------------------------------------------
