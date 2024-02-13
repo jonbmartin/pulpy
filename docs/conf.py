4e6104d327abcb9e42e174d23e7c3481450114ce
jonbmartin <jon.bach.martin@gmail.com>
2024-02-13 15:48:46 -0600

    change theme

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "pyramid"
 
 html_logo = ""
 
