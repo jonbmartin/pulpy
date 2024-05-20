9f48ef22d3fd16106fa661c37ca48a9657cdfe66
jonbmartin <jon.bach.martin@gmail.com>
2024-05-20 13:43:40 -0500

    add discovery of tests to shell script

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,6 @@ rm -rf docs/generated/
 black .
 isort .
 ruff .
-coverage run -m unittest
+python -m unittest discover tests
+coverage run --source=./tests -m unittest discover -s tests/
 sphinx-build docs -W -b linkcheck -d _build/doctrees _build/html
