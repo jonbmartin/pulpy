ab1976e5f56aa31c17de01c16549f100c3a5bd19
Jonathan Martin <44706383+jonbmartin@users.noreply.github.com>
2024-05-20 12:18:31 -0500

    Create test_optim.py

---
+++ tests/grad/test_optim.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class TestTrajGrad(unittest.TestCase):
+    def test_min_gradient(self):
+        t = np.linspace(0, 1, 1000)
+        kx = np.sin(2.0 * math.pi * t)
+        ky = np.cos(2.0 * math.pi * t)
+        kz = t
+        k = np.stack((kx, ky, kz), axis=-1)
+
+        (g, k, s, t) = waveform.min_time_gradient(
+            k, 0.0, 0.0, gmax=4, smax=15, dt=4e-3, gamma=4.257
+        )
+
+        npt.assert_almost_equal(np.max(t), 0.916, decimal=4)
